b1db4241c1497f7ff39eb9f66671424c5632263b
Federico-Benitez <fedebenitez1996@gmail.com>
2021-05-27 20:25:55 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Hola mundo</Text>
+    </View>
+  );
+};
+
+export default App;
