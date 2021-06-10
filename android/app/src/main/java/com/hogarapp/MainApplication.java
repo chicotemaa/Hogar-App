a0863c9565159f3524ff7f591d0fc6d62c63478e
federico-benitez <fedebenitez1996@gmail.com>
2021-06-10 16:09:34 -0300

    animaciones cargadas y reanimated 2 instalado

--- android/app/src/main/java/com/hogarapp/MainApplication.java
+++ android/app/src/main/java/com/hogarapp/MainApplication.java
@@ -13,8 +13,8 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-  import com.facebook.react.bridge.JSIModulePackage; // <- add
-  import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
+import com.facebook.react.bridge.JSIModulePackage; // <- add
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -39,10 +39,10 @@ public class MainApplication extends Application implements ReactApplication {
           return "index";
         }
 
-        // @Override
-        // protected JSIModulePackage getJSIModulePackage() {
-        //   return new ReanimatedJSIModulePackage(); // <- add
-        // }
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage(); // <- add
+        }
       };
 
   @Override
