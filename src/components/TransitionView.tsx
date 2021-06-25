802e94ac7f5f816699f288d39fa997dd53bbbe45
federico-benitez <fedebenitez1996@gmail.com>
2021-06-25 14:50:58 -0300

    Componente modularizado de transition

---
+++ src/components/TransitionView.tsx
@@ -0,0 +1,21 @@
+import React, {PureComponent} from 'react';
+import {View} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import {theme} from '../theme/appTheme';
+
+type PropsType = {index?: number};
+
+export class TransitionView extends PureComponent<PropsType> {
+  render() {
+    const {index, ...rest} = this.props;
+    return (
+      <Animatable.View
+        animation="fadeIn"
+        duration={theme.transitionDuration}
+        delay={index ? (index * theme.transitionDuration) / 5 : 0}
+        useNativeDriver
+        {...rest}
+      />
+    );
+  }
+}
