e76d7b28c47d943de50f9f48688da760aa71c9ed
federico-benitez <fedebenitez1996@gmail.com>
2021-07-15 15:44:56 -0300

    THOR-13 spinner de carga welcome

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -5,10 +5,13 @@ import { Header } from '../components/Header';
 import { getToken, getUserInfo } from '../api/api';
 import { TecnicosWelcomeScreen } from '../components/Welcome/TecnicosWelcomeScreen';
 import { WelcomeOptions } from '../components/Welcome/WelcomeOptions';
+import { View } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 interface Props extends DrawerScreenProps<any, any> { }
 
 export const WelcomeScreen = ({ navigation }: Props) => {
+  const [loading, setLoading] = useState(true);
   const [userName, setUserName] = useState('');
   const [roleUser, setRoleUser] = useState('');
 
@@ -16,6 +19,7 @@ export const WelcomeScreen = ({ navigation }: Props) => {
     getToken().then(token => {
       getUserInfo(token).then(response => {
         const { username, roles } = response.data;
+        console.log(response.data)
         setUserName(capitalizeFirstLetter(username));
 
         if (roles[1] === 'ROLE_EMPLEADO') {
@@ -23,6 +27,10 @@ export const WelcomeScreen = ({ navigation }: Props) => {
         } else {
           setRoleUser('user')
         }
+
+        setTimeout(() => {
+          setLoading(false)
+        }, 500)
       });
     });
 
@@ -31,20 +39,33 @@ export const WelcomeScreen = ({ navigation }: Props) => {
     });
   }, []);
 
-
-
   return (
     <>
-      <Header pageName="Bienvenido" userName={userName} roleUser={roleUser} />
-      {roleUser === 'tecnico' ? (
-        <TecnicosWelcomeScreen navigation={navigation} />) :
-        (<WelcomeOptions navigation={navigation} />)
-      }
+      {loading ? (
+        <View>
+          <Spinner
+            visible={loading}
+            textContent={'Cargando...'}
+            textStyle={{ color: '#FFF' }}
+          />
+        </View>
+      ) : (
+        <>
+          <Header pageName="Bienvenido" userName={userName} roleUser={roleUser} />
+          {
+            roleUser === 'tecnico' ? (
+              <TecnicosWelcomeScreen navigation={navigation} />
+            ) : (
+              <>
+                <WelcomeOptions navigation={navigation} />
+              </>
+            )
+          }
+        </>
+      )}
     </>
   );
 };
-
-
 
 function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
