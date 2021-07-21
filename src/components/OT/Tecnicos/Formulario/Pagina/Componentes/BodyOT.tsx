255a09057ba62e2ae9e1a3b8d1bf8e2bfd1c9fce
federico-benitez <fedebenitez1996@gmail.com>
2021-07-21 14:16:25 -0300

    THOR-30 paginador agregado

--- src/components/OT/Tecnicos/Formulario/Pagina/Componentes/BodyOT.tsx
+++ src/components/OT/Tecnicos/Formulario/Pagina/Componentes/BodyOT.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import { ScrollView } from 'react-native';
 import { StyleSheet, View, Text } from 'react-native';
 import { Button } from 'react-native-paper';
-import { Formulario } from '../interfaces';
-import { Modulo } from './Modulo';
+import { Formulario, Modulo } from '../interfaces';
+import { Pagina } from './Pagina';
 
 interface Props {
   Formulario: Formulario;
@@ -13,24 +13,24 @@ const PAGES = ['hola', 'hola', 'hola', 'holas']
 export const BodyOT = ({ Formulario }: Props) => {
   const [currentPage, setCurrentPage] = React.useState<number>(0);
   const [pagesCount, setPagesCount] = React.useState<number>(0)
-
-
+  const [pages, setPages] = React.useState<{ pagina: number, modulos: [] }[]>()
 
   React.useEffect(() => {
     const lastItemIndex = Formulario.propiedadModulos.length - 1
     const lastPage = Formulario.propiedadModulos[lastItemIndex]
-    setPagesCount(lastPage.pagina || 0)
+    setPagesCount(lastPage.pagina)
+    setCurrentPage(Formulario.propiedadModulos[0].pagina)
+    const paginador = crearPaginador(Formulario.propiedadModulos)
+    setPages(paginador)
     console.log('formulario', Formulario)
+    console.log('paginador', paginador)
   }, [])
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {Formulario.propiedadModulos.map(modulo => {
-          console.log(modulo.id)
-          return (<Modulo Modulo={modulo} />)
-        })}
-      </ScrollView>
+      <View>
+        <Text>{pages ? ('Si hay formulario') : ('No hay formulario')}</Text>
+      </View>
 
       <View style={{ flexDirection: 'row', justifyContent: 'space-between', margin: 10 }}>
         <Button mode="text" onPress={() => currentPage > 0 ? setCurrentPage(currentPage - 1) : null}>
@@ -44,6 +44,35 @@ export const BodyOT = ({ Formulario }: Props) => {
   );
 }
 
+
+const crearPaginador = (Modulos: Modulo[]) => {
+  let paginaActual: number = Modulos[0].pagina
+  let arrayModulos: Modulo[] = []
+  let paginador: {
+    pagina: number,
+    modulos: Modulo[]
+  }[] = []
+
+  Modulos.forEach((modulo: Modulo) => {
+    if (modulo.pagina == paginaActual) {
+      arrayModulos.push(modulo)
+    } else {
+      paginador.push({
+        pagina: paginaActual,
+        modulos: arrayModulos
+      })
+      arrayModulos = []
+      arrayModulos.push(modulo)
+      paginaActual = modulo.pagina
+    }
+  })
+  //ultima pagina
+  paginador.push({
+    pagina: paginaActual,
+    modulos: arrayModulos
+  })
+  return paginador
+}
 
 
 
