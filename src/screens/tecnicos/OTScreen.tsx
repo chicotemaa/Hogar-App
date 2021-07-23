6209eca67cc7ee530e0f2c1f64d991b69ecdb172
federico-benitez <fedebenitez1996@gmail.com>
2021-07-23 14:45:18 -0300

    OT screen con botones de firmado y postergar

--- src/screens/tecnicos/OTScreen.tsx
+++ src/screens/tecnicos/OTScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { ScrollView, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import { Header } from '../../components/Header'
 import { styles } from '../../theme/appTheme'
 import { StackScreenProps } from '@react-navigation/stack'
@@ -12,29 +12,8 @@ interface Props
     extends StackScreenProps<any, any> { }
 
 
-interface PropiedadItem {
-    id: number;
-    requerido: boolean;
-    item: Item;
-}
-
-interface Item {
-    descripcion: string;
-    nombre: string;
-    id: number;
-    tipo: string;
-    opciones: Array<any>;
-}
-
 export const OTScreen = ({ navigation, route }: Props) => {
     const { OT } = route.params
-    const [modulos, setModulos] = useState([])
-
-    useEffect(() => {
-        getFormularioAPI(OT.formulario.id).then(formulario => {
-            setModulos(formulario.propiedadModulos)
-        })
-    }, [])
 
     return (
         <>
@@ -42,12 +21,10 @@ export const OTScreen = ({ navigation, route }: Props) => {
             <View style={[styles.container, { paddingHorizontal: 0 }]}>
                 <BasePage OrdenTrabajo={OT} />
             </View>
+            <View>
+                <Text>Button</Text>
+            </View>
         </>
     )
-}
-
-
-const buildItem = (item: PropiedadItem) => {
-    return (<BaseCampo item={item} />)
 }
 
