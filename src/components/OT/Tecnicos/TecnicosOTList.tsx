ae88912cc8f3d0b678df564d871d74f669a3e248
federico-benitez <fedebenitez1996@gmail.com>
2021-07-29 12:48:11 -0300

    error de vista welcome Text fixeado

--- src/components/OT/Tecnicos/TecnicosOTList.tsx
+++ src/components/OT/Tecnicos/TecnicosOTList.tsx
@@ -80,7 +80,7 @@ export const TecnicosOTList = () => {
                                             stackNavigator.navigate('DetalleOTScreen')
                                         }
                                     }}
-                                /> </TransitionView>
+                                /></TransitionView>
                         )
                     })
                 }
