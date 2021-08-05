f6fceed00354dfa89cb01e8b4454ba7416ba4b5f
federico-benitez <fedebenitez1996@gmail.com>
2021-08-05 14:45:22 -0300

    margin bottom en ot list para ios

--- src/components/OT/Tecnicos/Formulario/Pagina/BasePage.tsx
+++ src/components/OT/Tecnicos/Formulario/Pagina/BasePage.tsx
@@ -11,6 +11,7 @@ import Spinner from 'react-native-loading-spinner-overlay';
 import { useNavigation } from '@react-navigation/native'
 import { changeStateFinalizado } from '../../../../../services/tecnicosServices'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Platform } from 'react-native'
 
 interface Props {
     OrdenTrabajo: OrdenTrabajo,
@@ -68,38 +69,37 @@ export const BasePage = ({ OrdenTrabajo, hasResultado }: Props) => {
                 </View>
             ) : (
 
-                <SafeAreaView style={{ flex: 1 }}>
-                    <View style={styles.page}>
-                        <Encabezado OrdenTrabajo={OrdenTrabajo} />
-                        <View style={{ flex: 1 }}>
-                            <Portal>
-                                <Dialog visible={visible} onDismiss={hideDialog}>
-                                    <Dialog.Title>Alert</Dialog.Title>
-                                    <Dialog.Content>
-                                        <Text>Firma</Text>
-                                    </Dialog.Content>
-                                    <Dialog.Actions>
-                                        <Button onPress={finalizadoHandler}>Done</Button>
-                                    </Dialog.Actions>
-                                </Dialog>
-                            </Portal>
-                            <FormState>
-                                {formulario ? <BodyOT Formulario={formulario} /> : null}
-                            </FormState>
-                        </View>
-                        <View style={styles.footer}>
-                            <Button mode="text" icon="clock" labelStyle={{ color: 'red' }} onPress={postergarHandler}>
-                                Postergar
-                            </Button>
-                            <Button mode="text" icon="draw" labelStyle={{ color: 'blue' }} onPress={showDialog}>
-                                Firmar
-                            </Button>
-                            <Button mode="text" labelStyle={{ color: 'green' }} onPress={guardarHandler}>
-                                Guardar
-                            </Button>
-                        </View>
+                <View style={styles.page}>
+                    <Encabezado OrdenTrabajo={OrdenTrabajo} />
+                    <View style={{ flex: 1 }}>
+                        <Portal>
+                            <Dialog visible={visible} onDismiss={hideDialog}>
+                                <Dialog.Title>Alert</Dialog.Title>
+                                <Dialog.Content>
+                                    <Text>Firma</Text>
+                                </Dialog.Content>
+                                <Dialog.Actions>
+                                    <Button onPress={finalizadoHandler}>Done</Button>
+                                </Dialog.Actions>
+                            </Dialog>
+                        </Portal>
+                        <FormState>
+                            {formulario ? <BodyOT Formulario={formulario} /> : null}
+                        </FormState>
                     </View>
-                </SafeAreaView>
+                    <View style={styles.footer}>
+                        <Button mode="text" icon="clock" labelStyle={{ color: 'red' }} onPress={postergarHandler}>
+                            Postergar
+                        </Button>
+                        <Button mode="text" icon="draw" labelStyle={{ color: 'blue' }} onPress={showDialog}>
+                            Firmar
+                        </Button>
+                        <Button mode="text" labelStyle={{ color: 'green' }} onPress={guardarHandler}>
+                            Guardar
+                        </Button>
+                    </View>
+                </View>
+
             )
             }</>
     )
@@ -108,7 +108,9 @@ export const BasePage = ({ OrdenTrabajo, hasResultado }: Props) => {
 const styles = StyleSheet.create({
     page: {
         flex: 1,
-        // marginVertical: 0.005 * windowHeight,
+        marginTop: 0.005 * windowHeight,
+        marginBottom: Platform.OS === 'ios' ? windowHeight * 0.025 : 0,
+        alignItems: 'stretch',
         marginHorizontal: 0.005 * windowWidth,
     },
     footer: {
