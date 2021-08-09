dd502c8d169de0f6f9692d82d5725ad29577ed49
federico-benitez <fedebenitez1996@gmail.com>
2021-08-09 12:32:45 -0300

    removed unused imports from OT details screen

--- src/screens/tecnicos/OTScreen.tsx
+++ src/screens/tecnicos/OTScreen.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { View } from 'react-native'
 import { Header } from '../../components/Header'
-import { styles } from '../../theme/appTheme'
 import { StackScreenProps } from '@react-navigation/stack'
 import { BasePage } from '../../components/OT/Tecnicos/Formulario/Pagina/BasePage'
 import { OrdenTrabajo } from '../../services/interfaces'
 
 interface Props
     extends StackScreenProps<any, any> { }
-
-
-
 
 export const OTScreen = ({ navigation, route }: Props) => {
     const OT: OrdenTrabajo = route.params.OT
