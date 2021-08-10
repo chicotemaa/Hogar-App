af29822a339a6c430a41adf8069128c79606fdee
federico-benitez <fedebenitez1996@gmail.com>
2021-08-10 14:45:49 -0300

    ejemplo publicacion de resultado

--- src/api/apiTecnicos.tsx
+++ src/api/apiTecnicos.tsx
@@ -58,3 +58,68 @@ export const changeStateOrdenTrabajo = async (ordenTrabajo: any, data: any) => {
     })
 }
 
+export const postResultado = async () => {
+    let headers = {
+        'Content-Type' : 'application/ld+json',
+        'Authorization' : 'Bearer ' + await getData('access_token')
+    }
+
+    const data = {
+        resultados:[
+           {
+              valor:[
+                 "asd"
+              ],
+              propiedadItem:"/api/propiedad_items/2444",
+              imageName:"string",
+              imageSize:0,
+              latitud:"2",
+              longitud:"3",
+              indiceItem:0,
+              indiceModulo:0,
+              idModulo:0,
+              isColeccionable:false
+           },{
+            valor:[
+               847
+            ],
+            propiedadItem:"/api/propiedad_items/2459",
+            imageName:"string",
+            imageSize:0,
+            latitud:"2",
+            longitud:"3",
+            indiceItem:0,
+            indiceModulo:0,
+            idModulo:0,
+            isColeccionable:false
+         },
+         {
+            valor:[
+               847
+            ],
+            propiedadItem:"/api/propiedad_items/2459",
+            imageName:"string",
+            imageSize:0,
+            latitud:"2",
+            longitud:"3",
+            indiceItem:0,
+            indiceModulo:0,
+            idModulo:0,
+            isColeccionable:false
+         },
+        ],
+        latitud:"12345",
+        longitud:"000000",
+        ordenTrabajo:"/api/orden_trabajos/4887",
+        minutosTrabajado:16,
+        minutosReales:6
+     }
+
+    return fetch(baseApi+'/formulario_resultados',{
+        method:'POST',
+        headers,
+        body: JSON.stringify(data)
+    }).then((response) => {
+        console.log(response)
+    })
+}
--- src/services/ResultadoServices.ts
+++ src/services/ResultadoServices.ts
@@ -1,29 +1,74 @@
 import { Formulario, Item, ModuloProps } from "./interfaces";
 
-export const buildResult = (formulario : Formulario) => {
-    const {propiedadModulos, id} = formulario;
-    
+export const buildResult = (formulario: Formulario) => {
+    const { propiedadModulos, id } = formulario;
+
     return {
         id,
-        modulos : propiedadModulos.map(({id,  modulo})=> {
-            console.log('modulo', modulo, id)
-            return getModulo(modulo)
+        modulos: propiedadModulos.map(({ id, modulo }) => {
+            return {
+                moduloId: id,
+                moduloItems: getModulo(modulo)
+            }
         })
     }
 }
 
-const getModulo = ({id}: ModuloProps) => {
+const getModulo = (modulo: ModuloProps) => {
     return {
-        id:id,
+        id: modulo.id,
+        items: getItems(modulo.propiedadItems)
     }
 }
 
-const getItems = (Items:Item[]) => {
-    return Items.map((item)=> {
-        return getItemValue(item)
+const getItems = (Items: Item[]) => {
+    return Items.map(item => {
+        return {
+            id: item.id,
+            itemProps: getItemProps(item)
+        }
     })
+
 }
 
-const getItemValue = (Item) => {
-    return Item.id
+const getItemProps = (Item) => {
+    //console.log('esto tiene item', Item)
+    return {
+        id: Item.id,
+        tipo: Item.item.tipo,
+        requerido: Item.requerido,
+        opciones: Item.item.opciones,
+        cantidadMinima: Item.cantidadMinima,
+        value: null,
+    }
+}
+
+
+export const getResults = (values) => {
+    console.log(values)
+}
+
+export const postResultado = () => {
+    const body = {
+        resultados: [{
+            valor: [
+                "resultado prueba"
+            ],
+            propiedadItem: "api/propiedad_items/2444",
+            imageName: "string",
+            imageSize: 0,
+            latitud: "123",
+            longitud: "456",
+            indiceItem: 0,
+            indiceModulo: 0,
+            idModulo: 0,
+            isColeccionable: false
+        }
+        ],
+        latitud: '12345',
+        longitud: '000000',
+        ordenTrabajo: '/api/orden_trabajos/4897',
+        minutosTrabajados: 16,
+        minutosReales: 6
+    }
 }
