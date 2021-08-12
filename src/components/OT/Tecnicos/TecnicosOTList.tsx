682db1bd48b63f2f2f90f4dab6c237c1c85e345c
federico-benitez <fedebenitez1996@gmail.com>
2021-08-12 11:25:58 -0300

    THOR-86 refresh contron con refetch query y componentes modularizados

--- src/components/OT/Tecnicos/TecnicosOTList.tsx
+++ src/components/OT/Tecnicos/TecnicosOTList.tsx
@@ -1,82 +1,38 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { ScrollView, View, RefreshControl, Text } from 'react-native';
-import { useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 import { OrdenTrabajo } from '~/services/interfaces';
 import { getOrdenesTrabajoInfo } from '~/services/tecnicosServices';
 import { ItemOT } from '~/components/ItemOT';
 import { TransitionView } from '~/components/TransitionView';
 
 export const TecnicosOTList = () => {
-  const queryClient = useQueryClient();
-  const { data, error, isFetching } = useQuery('OTList', getOrdenesTrabajoInfo);
-
-  const [refreshing, setRefreshing] = React.useState(false);
+  const { data, error, isFetching, refetch } = useQuery(
+    'OTList',
+    getOrdenesTrabajoInfo,
+  );
   const stackNavigator = useNavigation();
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    queryClient.refetchQueries('OTList');
-    setRefreshing(false);
-  }, []);
-
-  if (error) {
-    return (
-      <View>
-        <Text>Error al obtener el listado de las ot {error}</Text>
-      </View>
-    );
-  }
-
-  if (isFetching) {
-    return (
-      <View>
-        <Text>Cargando ot </Text>
-      </View>
-    );
-  }
 
   return (
     <View style={{ flex: 1 }}>
       <ScrollView
         refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          <RefreshControl refreshing={isFetching} onRefresh={refetch} />
         }>
-        {data &&
-          (data.length > 0 ? (
+        {data && !isFetching ? (
+          data.length > 0 ? (
             data.map((OT: OrdenTrabajo) => {
-              return (
-                <TransitionView
-                  key={OT.id}
-                  animation="slideInUp"
-                  index={0}
-                  isOT>
-                  <ItemOT
-                    OT={OT}
-                    id={OT.id}
-                    estadoOT={OT.estado}
-                    cliente={OT.cliente.razonSocial}
-                    titulo={OT.formulario.titulo}
-                    location={OT.direccionSucursalCliente}
-                    date={OT.fecha}
-                    rol="tecnico"
-                    horaDesde={OT.horaDesde}
-                    horaHasta={OT.horaHasta}
-                    goToScreen={(estado: string) => {
-                      if (estado === 'realizarOT') {
-                        //TODO: controlar ubicacion
-                        stackNavigator.navigate('OTScreen', { OT });
-                      } else if ((estado = 'detalleOTRealizada')) {
-                        stackNavigator.navigate('DetalleOTScreen');
-                      }
-                    }}
-                  />
-                </TransitionView>
-              );
+              return <ListItem OT={OT} stackNavigator={stackNavigator} />;
             })
           ) : (
             <EmptyList />
-          ))}
+          )
+        ) : isFetching ? (
+          <LoadingMessage />
+        ) : (
+          <ErrorMessage />
+        )}
       </ScrollView>
     </View>
   );
@@ -84,4 +40,40 @@ export const TecnicosOTList = () => {
 
 const EmptyList = () => {
   return <View style={{ flex: 1 }}>{<Text>No hay ot pendientes</Text>}</View>;
+};
+
+const ListItem = ({ OT, stackNavigator }) => {
+  return (
+    <TransitionView key={OT.id} animation="slideInUp" index={0} isOT>
+      <ItemOT
+        OT={OT}
+        titulo={OT.formulario.titulo}
+        rol="tecnico"
+        goToScreen={(estado: string) => {
+          if (estado === 'realizarOT') {
+            //TODO: controlar ubicacion
+            stackNavigator.navigate('OTScreen', { OT });
+          } else if ((estado = 'detalleOTRealizada')) {
+            stackNavigator.navigate('DetalleOTScreen');
+          }
+        }}
+      />
+    </TransitionView>
+  );
+};
+
+const LoadingMessage = () => {
+  return (
+    <View>
+      <Text>Cargando ot </Text>
+    </View>
+  );
+};
+
+const ErrorMessage = () => {
+  return (
+    <View>
+      <Text>Error al obtener el listado de las ot {}</Text>
+    </View>
+  );
 };
