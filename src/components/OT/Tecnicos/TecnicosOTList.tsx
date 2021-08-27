463a711612790df126121d072f87e479339cbd53
federico-benitez <fedebenitez1996@gmail.com>
2021-08-27 11:50:14 -0300

    param otDetalle

--- src/components/OT/Tecnicos/TecnicosOTList.tsx
+++ src/components/OT/Tecnicos/TecnicosOTList.tsx
@@ -63,7 +63,7 @@ const ListItem = ({ OT, stackNavigator }) => {
             //TODO: controlar ubicacion
             stackNavigator.navigate('OTScreen', { OT });
           } else if ((estado = 'detalleOTRealizada')) {
-            stackNavigator.navigate('DetalleOTScreen');
+            stackNavigator.navigate('DetalleOTScreen', { OT });
           }
         }}
       />
