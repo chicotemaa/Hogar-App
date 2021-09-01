2d83ccf8208dbb5d1b04761fbc98a15fbb9be801
federico-benitez <fedebenitez1996@gmail.com>
2021-09-01 15:52:42 -0300

    Listado ot detalles fixed

--- src/screens/ListadoOTScreen.tsx
+++ src/screens/ListadoOTScreen.tsx
@@ -2,22 +2,32 @@ import React from 'react';
 import { View, ScrollView } from 'react-native';
 import { Header } from '~/components/Header';
 import { ItemOT } from '~/components/ItemOT';
+import { useNavigation } from '@react-navigation/native';
 
-export const ListadoOTScreen = ({ navigation }) => {
+export const ListadoOTScreen = () => {
+  const OT = {
+    id: 3,
+    location: 'Sarmiento 123',
+    cliente: {
+      razonSocial: 'Santander',
+    },
+    fecha: '12 agosto 2020',
+    estado: 4,
+    horaDesde: '2021-03-31T15:30',
+    horaHasta: '2021-03-31T15:30',
+  };
+  const navigation = useNavigation();
+
   return (
     <>
       <Header pageName="Ordenes de Trabajo" />
       <View style={{ flex: 8 }}>
         <ScrollView>
           <ItemOT
-            estadoOT={1}
-            id={3}
+            OT={OT}
             titulo="Vidrio roto"
-            location="Sarmiento 123"
-            horaDesde="2021-03-31T15:30"
-            horaHasta="2021-03-31T15:30"
-            date="13 Agosto 2020"
             goToScreen={() => {
+              //TODO: pasar por params la OT correspondiente
               navigation.navigate('DetalleOTScreen');
             }}
           />
