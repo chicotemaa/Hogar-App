c7825695cfba4bdbfcbef3e07032e2aa24fa94c8
federico-benitez <fedebenitez1996@gmail.com>
2021-09-02 14:15:30 -0300

    THOR-143 opciones para ingresar dependiendo rol

---
+++ src/screens/administrador/HogarOptions.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const HogarOptions = () => {
+  return (
+    <View>
+      <Text>Hola</Text>
+    </View>
+  );
+};
